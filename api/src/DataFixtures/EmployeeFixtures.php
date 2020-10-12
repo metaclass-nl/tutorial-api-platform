eb6a54b498022175fcad0b554e4c2b74a2803278
henk <info@metaclass.nl>
2020-10-12 14:59:29 +0200

    did composer update Adapted EmployeeFixture Adapted HourFixture Adapted SimpleSearchFilter

--- api/src/DataFixtures/EmployeeFixtures.php
+++ api/src/DataFixtures/EmployeeFixtures.php
@@ -4,7 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Employee;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
 class EmployeeFixtures extends Fixture implements DependentFixtureInterface
