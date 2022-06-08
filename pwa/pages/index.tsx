301ba3ed3938773bbd4c4c1ed53bb016deaefa19
henk <info@metaclass.nl>
2022-06-08 16:46:01 +0200

    - did composer update - adapted welcome page

--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -29,7 +29,7 @@ const Welcome = () => (
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://github.com/metaclass-nl/tutorial-api-platform/blob/chapter2-api/README.md"
+            href="https://github.com/metaclass-nl/tutorial-api-platform/blob/chapter3-api/README.md"
             className="main__button"
           >
             Get started<Arrow />
